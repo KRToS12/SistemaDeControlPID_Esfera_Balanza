61910829abfd653e336f6525151dd59ed3e2fc31
Brian <brianz1406b@gmail.com>
2025-11-08 18:06:39 -0400

    Mejorando servo y PID

---
+++ Ultrasonico.ino
@@ -0,0 +1,106 @@
+#include <Servo.h>
+
+
+#define TRIG_PIN 9 
+#define ECHO_PIN 8 
+
+
+
+float setpoint = 15.0; 
+float distancia = 0;
+float error, error_anterior = 0;
+float integral = 0;
+float derivada = 0;
+float salida = 0;
+
+float Kp = 8;     
+float Ki = 0.2;   
+float Kd = 3100; 
+
+unsigned long tiempo = 0;
+unsigned long tiempo_anterior = 0;
+unsigned long periodo = 50; 
+
+Servo servo;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(10);
+  
+  
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  
+  Serial.println("Sistema PID Balanza y Bola Iniciado");
+}
+
+float medirDistancia() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duracion = pulseIn(ECHO_PIN, HIGH, 20000); 
+
+  float distancia;
+
+  
+  if (duracion == 0) {
+    distancia = -1; 
+  } else {
+    distancia = duracion * 0.034 / 2; 
+  }
+
+  return distancia; 
+}
+
+void loop() {
+  tiempo = millis();
+  if (tiempo > tiempo_anterior + periodo) {
+    tiempo_anterior = tiempo;
+
+    distancia = medirDistancia();
+
+    if (distancia > 2 && distancia < 30) {
+
+      error = setpoint - distancia;
+
+
+      float PID_p = Kp * error;
+
+
+      float PID_d = Kd * ((error - error_anterior) / periodo);
+
+
+      if (-3 < error && error < 3) {
+        integral += Ki * error;
+      } else {
+        integral = 0;
+      }
+
+
+      salida = PID_p + integral + PID_d;
+
+
+      salida = map(salida, -150, 150, 0, 150);
+
+      if (salida < 20) salida = 20;
+      if (salida > 160) salida = 160;
+
+      float angulo = salida + 30;
+      servo.write(angulo);
+
+      Serial.print("Distancia: ");
+      Serial.print(distancia);
+      Serial.print(" cm | Error: ");
+      Serial.print(error);
+      Serial.print(" | Salida PID: ");
+      Serial.print(salida);
+      Serial.print(" | Angulo Servo: ");
+      Serial.println(angulo);
+    }
+
+    error_anterior = error;
+  }
+}
